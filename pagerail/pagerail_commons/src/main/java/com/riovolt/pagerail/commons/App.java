8ee18a067473e7cd2c915fbc927355b281af9abe
vargeji-PC\vargeji <developer.akshay.barge@gmail.com>
2017-02-04 23:26:19 +0530

    commiting new project structure

---
+++ pagerail/pagerail_commons/src/main/java/com/riovolt/pagerail/commons/App.java
@@ -0,0 +1,13 @@
+package com.riovolt.pagerail.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pagerail/pagerail_exception/src/main/java/com/riovolt/pagerail/exceptions/App.java
@@ -0,0 +1,13 @@
+package com.riovolt.pagerail.exceptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
